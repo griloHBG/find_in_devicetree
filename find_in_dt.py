282c2160f33deb06e276ce1d77f9b1698c0ecd09
henrique.garcia <henrique.garcia@toradex.com>
2021-08-28 12:17:31 -0300

    Some cosmetics

--- find_in_dt.py
+++ find_in_dt.py
@@ -1,7 +1,10 @@
 #! /bin/python3
 
 '''
-This script supports both arm and arm64 architectures. Dependency: GitPython (https://gitpython.readthedocs.io/en/stable/intro.html)
+This script supports both arm and arm64 architectures.
+Dependencies:
+- GitPython (https://gitpython.readthedocs.io/en/stable/intro.html) - python3 -m pip install
+- Colorit (https://gitpython.readthedocs.io/en/stable/intro.html)
 
 This script should run inside a valid Linux git (like the linux-toradex)
 
@@ -19,17 +22,17 @@ or
 
 '''
 
-import os
-from typing import List
-
-from git import Repo, InvalidGitRepositoryError
-from pathlib import Path
 import re
+import warnings
+from pathlib import Path
+from typing import List, Union
+
 from colorit import *
-import sys
+from git import Repo, InvalidGitRepositoryError
+
 
 class MyRepository:
-    def __init__(self, repo:Repo , arch:str='arm'):
+    def __init__(self, repo: Repo, arch: str = 'arm'):
         self._arch = arch
         self._repo = repo
 
@@ -39,8 +42,9 @@ class MyRepository:
     def active_branch(self):
         return self._repo.active_branch
 
+
 class SearchHit:
-    def __init__(self, file_name:Path):#, line_number:int, before_hit:str, hit:str, after_hit:str):
+    def __init__(self, file_name: Path):  # , line_number:int, before_hit:str, hit:str, after_hit:str):
         self.file_name = file_name
         self.line_number = []
         self.before_hit = []
@@ -51,12 +55,13 @@ class SearchHit:
 
     def to_dict(self):
         return {'file_name': self.file_name,
-                'hits':{line_number:[before, hit, after, hit_start, hit_end]
-                        for line_number, before, hit, after, hit_start, hit_end
-                        in zip(self.line_number, self.before_hit, self.hit, self.after_hit, self.hit_start, self.hit_end)}
+                'hits': {line_number: [before, hit, after, hit_start, hit_end]
+                         for line_number, before, hit, after, hit_start, hit_end
+                         in
+                         zip(self.line_number, self.before_hit, self.hit, self.after_hit, self.hit_start, self.hit_end)}
                 }
 
-    def append(self, line_number:int, before_hit:str, hit:str, after_hit:str, hit_start:int, hit_end:int):
+    def append(self, line_number: int, before_hit: str, hit: str, after_hit: str, hit_start: int, hit_end: int):
         self.line_number.append(line_number)
         self.before_hit.append(before_hit)
         self.hit.append(hit)
@@ -64,8 +69,8 @@ class SearchHit:
         self.hit_start.append(hit_start)
         self.hit_end.append(hit_end)
 
-def get_all_includes(dts_file_path, include_dir, dts_path):
 
+def get_all_includes(dts_file_path, include_dir, dts_path):
     if not include_dir.exists() or not include_dir.is_dir():
         raise LookupError(f'Include directory {include_dir} doesn\'t exist or isn\'t a file!')
 
@@ -89,10 +94,10 @@ def get_all_includes(dts_file_path, include_dir, dts_path):
         if f[-1] == 'h':
             if not f in include_file_list:
                 include_file_list.append(f)
-                #search = re.search("pinfunc", f, re.IGNORECASE)
-                #if search: # pinfunc headers are within the same folder as dts files
+                # search = re.search("pinfunc", f, re.IGNORECASE)
+                # if search: # pinfunc headers are within the same folder as dts files
                 #    include_path_list.append(dts_path / f)
-                #else: # non pinfunc are in linux-kernel/include/dt-bindings
+                # else: # non pinfunc are in linux-kernel/include/dt-bindings
                 #    include_path_list.append(include_dir / f)
                 if (include_dir / f).exists():
                     include_path_list.append(include_dir / f)
@@ -137,14 +142,14 @@ def get_repository(file:Path):
     return git
 
 
-def get_architecture(dts_file_path:Path):
-
+def get_architecture(dts_file_path: Path):
     arch = ''
     search = re.search(r'arch/([\w\d]+)', str(dts_file_path))
     if search:
         arch = search.group(1)
     else:
-        raise LookupError(f'\nYou are in {dts_file_path}, which doesn\'t define an architecture. Please cd into a dts folder.')
+        raise LookupError(
+            f'\nYou are in {dts_file_path}, which doesn\'t define an architecture. Please cd into a dts folder.')
 
     return arch
 
@@ -177,12 +182,11 @@ def find_in_dt(dts_file_path:Path, search_string_list):# -> tuple[MyRepository, 
 
     repo = MyRepository(linux_git, arch)
     try:
-        print('active git branch:',color(repo.active_branch(), Colors.red), '\n')
+        print('active git branch:', color(repo.active_branch(), Colors.red), '\n')
     except TypeError as e:
-        print('active git branch:',color("no branch found", Colors.red), '\n')
+        print('active git branch:', color("no branch found", Colors.red), '\n')
     except Exception as e:
         raise e
-
 
     include_dir = Path(linux_git.working_dir) / 'include'
     try:
@@ -219,28 +223,32 @@ def find_in_dt(dts_file_path:Path, search_string_list):# -> tuple[MyRepository, 
         with open(file) as f:
             content_lines = f.readlines()
 
-            new_search_hit = SearchHit(file)
-            something_found = False
-            character_sum = 0
-            for line_number, line_text in enumerate(content_lines):
-                for ss in search_string_list:
-                    #TODO: findall aqui? achar mais de uma vez a mesma coisa na mesma linha?!
-                    search = re.search(ss, line_text, re.IGNORECASE)
-                    if search:
-                        something_found = True
-                        span = search.span()
-                        new_search_hit.append(line_number+1, line_text[0:span[0]], line_text[span[0]:span[1]], line_text[span[1]:].rstrip(), character_sum+span[0], character_sum+span[1])
-                character_sum += len(line_text)
-            if something_found:
-                finds.append(new_search_hit)
+                new_search_hit = SearchHit(file)
+                something_found = False
+                character_sum = 0
+                for line_number, line_text in enumerate(content_lines):
+                    for ss in search_string_list:
+                        # TODO: findall aqui? achar mais de uma vez a mesma coisa na mesma linha?!
+                        search = re.search(ss, line_text, re.IGNORECASE)
+                        if search:
+                            something_found = True
+                            span = search.span()
+                            new_search_hit.append(line_number + 1, line_text[0:span[0]], line_text[span[0]:span[1]],
+                                                  line_text[span[1]:].rstrip(), character_sum + span[0],
+                                                  character_sum + span[1])
+                    character_sum += len(line_text)
+                if something_found:
+                    finds.append(new_search_hit)
 
     return finds, searched_files
 
 
 if __name__ == '__main__':
 
-    dt_file = sys.argv[1] # 'imx6dl-colibri-eval-v3.dts'
-    search_string_list = sys.argv[2:] # ['gpio2', 'MX6QDL_PAD_SD1_DAT']
+    print(Path.cwd())
+
+    dt_file = sys.argv[1]  # 'imx6dl-colibri-eval-v3.dts'
+    search_string_list = sys.argv[2:]  # ['gpio2', 'MX6QDL_PAD_SD1_DAT']
 
     finds, searched_files = find_in_dt(Path(dt_file), search_string_list)
 
@@ -258,7 +266,7 @@ if __name__ == '__main__':
 
 
 
-#for file in [*dts_path_list, *include_file_list]:
+# for file in [*dts_path_list, *include_file_list]:
 #    with open(file) as f:
 
 # property and value (has some dirty when multiple lines)
